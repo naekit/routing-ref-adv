636c0c4b4e11aa7c3928c0a8a919f2888a80b481
naekit <mykyta.onishchuck@code.berlin>
2023-01-17 15:07:32 -0500

    clear form with fetcher

--- frontend/src/components/NewsletterSignup.js
+++ frontend/src/components/NewsletterSignup.js
@@ -1,15 +1,29 @@
 import classes from "./NewsletterSignup.module.css"
+import { useFetcher } from "react-router-dom"
+import { useRef } from "react"
 
 function NewsletterSignup() {
+	const fetcher = useFetcher()
+	const formRef = useRef()
+
+	if (fetcher.state === "loading") {
+		formRef.current.reset()
+	}
+
 	return (
-		<form method="post" className={classes.newsletter}>
+		<fetcher.Form
+			method="post"
+			action="/newsletter"
+			className={classes.newsletter}
+			ref={formRef}
+		>
 			<input
 				type="email"
 				placeholder="Sign up for newsletter..."
 				aria-label="Sign up for newsletter"
 			/>
 			<button>Sign up</button>
-		</form>
+		</fetcher.Form>
 	)
 }
 
